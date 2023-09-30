d91111e137dc8c6011517c2e495ea94b414166b5
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-30 23:19:23 +0200

    feat: estabish routes for client pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/Homeage";
+import axios from "axios";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Dashbaord from "./pages/dashboard/Dashbaord";
 
 function App() {
-  const [count, setCount] = useState(0)
+  axios.defaults.baseURL = "http://localhost:5000";
+  axios.defaults.withCredentials = true;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashbaord />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
