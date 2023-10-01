585f8a4b7458bf3fd532263e881b5eaa139cbcb7
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-01 23:26:59 +0200

    chore: Add input validation using Zod

---
+++ api/src/utils/validation/userValidation.ts
@@ -0,0 +1,40 @@
+import { z, ZodError } from "zod";
+import { UserDocument } from "../../../../types";
+
+const registrationSchema = z.object({
+  firstName: z.string().min(3).max(100),
+  lastName: z.string().min(3).max(100),
+  email: z.string().email(),
+  password: z.string().min(7).max(100),
+});
+
+export const validateRegistrationInput = (data: UserDocument) => {
+  try {
+    const validData = registrationSchema.parse(data);
+    return { isValid: true, data: validData };
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return { isVlid: false, errors: error.errors };
+    }
+    // Handle unexpected error
+    throw error;
+  }
+};
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(7),
+});
+
+export const validateLoginInput = (email: string, password: string) => {
+  try {
+    const validData = loginSchema.parse({ email, password });
+    return { isValid: true, data: validData };
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return { isValid: false, errors: error.errors };
+    }
+
+    throw error;
+  }
+};
