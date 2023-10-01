0a87552071c29e898fcdc288ba1034e266157d48
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-01 11:19:33 +0200

    feat: implement user model

---
+++ api/src/user/model.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { UserDocument } from "../../../types";
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  role: { type: String, default: "user" },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const UserModel =
+  mongoose.models.User || mongoose.model<UserDocument>("User", userSchema);
+
+export default UserModel;
