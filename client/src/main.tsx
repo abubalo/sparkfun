3a39867f89dbf8cac419892fa0715c0a63c49620
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-01 12:38:08 +0200

    Wrapper framer 'AnimatePresence arround 'App' component in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AnimatePresence>
+        <App />
+      </AnimatePresence>
     </BrowserRouter>
   </React.StrictMode>
 );
