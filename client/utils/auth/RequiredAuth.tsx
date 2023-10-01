d9f094710bb00eef3974b5fc915daf4f60682cf7
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-01 10:28:19 +0200

    feat: implement required auth for user

---
+++ client/utils/auth/RequiredAuth.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/AuthProvider";
+import LoadingIndicator from "../../src/components/ui/LoadingIndicator";
+import { JSX } from "react/jsx-runtime";
+
+const RequiredAuth = (WrappedComponent: React.ComponentType<JSX.Element>) => {
+  return function WithAuth() {
+    const { user, loading } = useAuth();
+    const navigate = useNavigate();
+
+    const handleAuthentication = useCallback(() => {
+      if (!user) {
+        navigate("/login");
+      }
+    }, [user, navigate]);
+
+    useEffect(() => {
+      handleAuthentication();
+    }, [handleAuthentication]);
+
+    return loading ? (
+      <LoadingIndicator />
+    ) : (
+      <WrappedComponent key={null} type={undefined} props={undefined} />
+    );
+  };
+};
+
+export default RequiredAuth;
