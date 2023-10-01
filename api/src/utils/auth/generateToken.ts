3ed9e94c5616553e4010007c9f972235d2141dad
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-01 20:59:32 +0200

    feat: implement token generation for user

---
+++ api/src/utils/auth/generateToken.ts
@@ -0,0 +1,23 @@
+import { config } from "config/config";
+import * as jwt from "jsonwebtoken";
+import { UserDocument } from "../../../../types";
+
+export default async function generateToken(user: Partial<UserDocument>) {
+  try {
+    const payload = {
+      sub: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, config.jwt.secret, {
+      expiresIn: "24h",
+    });
+
+    return token;
+  } catch (error) {
+    throw new Error("Unable to generate token");
+  }
+}
