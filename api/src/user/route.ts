09e5d26b6c2a3d1199a4f5bf858666efa3af2581
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-02 21:50:03 +0200

    feat: implement route  helpers

---
+++ api/src/user/route.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import verifyTokenMiddleware from "middleware/verifyTokenMiddleware";
+
+import {
+  addUser,
+  deleteUser,
+  getUser,
+  updateUser,
+  loginUser,
+} from "./controller";
+
+const router = express.Router();
+
+router.post("/signup", addUser);
+router.post("/login", loginUser);
+
+// Attact verifyTokenMiddleware to routes that requires authentication
+router.get("/:userId", verifyTokenMiddleware, getUser);
+router.patch("/:userId", verifyTokenMiddleware, updateUser);
+router.delete("/:userId", verifyTokenMiddleware, deleteUser);
+
+const userRoutes = router;
+export default userRoutes;
