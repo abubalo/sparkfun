5d9156f29ba61c8097c609bb2fa8f974a794af58
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-02 15:11:32 +0200

    chore: add reqct query provider around App component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AnimatePresence>
-        <App />
-      </AnimatePresence>
+      <QueryClientProvider client={queryClient}>
+        <AnimatePresence>
+          <App />
+        </AnimatePresence>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
