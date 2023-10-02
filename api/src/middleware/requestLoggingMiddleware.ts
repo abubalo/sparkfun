8aeb34d1ec83cec3c08f00f222041f9e66d18342
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-02 22:21:36 +0200

    feat: Implement request log  helper function

---
+++ api/src/middleware/requestLoggingMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { Logging } from "utils/lib/Logging";
+
+// Middleware for request logging
+const requestLog = (req: Request, res: Response, next: NextFunction) => {
+  // Log the Request
+  Logging.info(
+    `Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
+
+  res.on("finish", () => {
+    // Log the Response
+    Logging.info(
+      `Outgoing -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`
+    );
+  });
+
+  next();
+};
+
+export default requestLog;
