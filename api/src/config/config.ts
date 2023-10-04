ff3a5ea7887131d221178077eab4d3ed5f383a18
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-04 23:41:52 +0200

    feat: add validation to enviroment variables

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -2,26 +2,60 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.pp0malq.mongodb.net`;
-
-const JWT_SECRET = process.env.JWT_SECRET || "";
-
-const SERVER_PORT = process.env.SERVER_PORT
-  ? Number(process.env.SERVER_PORT)
-  : 3500;
-
-export const config = {
+interface AppConfig {
   mongo: {
-    url: MONGO_URL,
-  },
-
+    url: string;
+  };
   server: {
-    port: SERVER_PORT,
-  },
+    port: number;
+  };
+  jwt: {
+    secret: string;
+  };
+  s3: {
+    accesskey: string;
+    secretKey: string;
+  };
+}
 
+// Define a function to validate and load environment variables
+function validateEnvVariable(
+  name: string,
+  defaultValue: string,
+  validator?: (value: string) => boolean
+): string {
+  const value = process.env[name] || defaultValue;
+  if (validator && !validator(value)) {
+    throw new Error(`Invalid value for ${name}: ${value}`);
+  }
+  return value;
+}
+
+// Validate and load environment variables
+const MONGO_USERNAME = validateEnvVariable("MONGO_USERNAME", "");
+const MONGO_PASSWORD = validateEnvVariable("MONGO_PASSWORD", "");
+const JWT_SECRET = validateEnvVariable("JWT_SECRET", "");
+const AWS_ACCESS_KEY = validateEnvVariable("AWS_ACCESS_KEY", "");
+const AWS_SECRET_KEY = validateEnvVariable("AWS_SECRET_KEY", "");
+
+const SERVER_PORT = validateEnvVariable("SERVER_PORT", "3500", (value) => {
+  const port = parseInt(value.toString(), 10);
+  return !isNaN(port) && port >= 1 && port <= 65535;
+});
+
+// Create the configuration object
+export const config: AppConfig = {
+  mongo: {
+    url: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.pp0malq.mongodb.net`,
+  },
+  server: {
+    port: parseInt(SERVER_PORT),
+  },
   jwt: {
     secret: JWT_SECRET,
+  },
+  s3: {
+    accesskey: AWS_ACCESS_KEY,
+    secretKey: AWS_SECRET_KEY,
   },
 };
