e745edce7b70bdc9133b7193f5f151d3d2ee742f
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-04 23:52:24 +0200

    feat: Create model for booking

---
+++ api/src/booking/model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { BookingDocument } from "../../../types";
+
+const bookingSchema = new mongoose.Schema({
+  user: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  talent: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  bookingDate: { type: Date, required: true },
+  occasion: { type: String, required: true },
+  plan: { type: String},
+  message: { type: String, required: true },
+  status: { type: String, default: "pending", required: true },
+  price: { type: Number, required: true },
+});
+
+const BookingModel =
+  mongoose.models.Booking ??
+  mongoose.model<BookingDocument>("Booking", bookingSchema);
+
+export default BookingModel;
