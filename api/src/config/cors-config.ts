ce43a4e9a55374fb17704784f13c3b01a2c654bd
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-22 23:20:34 +0200

    create cors config file

---
+++ api/src/config/cors-config.ts
@@ -0,0 +1,13 @@
+const corsConfig = {
+  allowedOrigins: process.env.ALLOWED_ORIGINS || [
+    "http://localhost:5173",
+    "https://sparkfun.vercel.app",
+  ],
+  allowedHeaders:
+    process.env.ALLOWED_HEADERS ||
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  allowedMethods:
+    process.env.ALLOWED_METHODS || "POST, PUT, PATCH, GET, DELETE",
+};
+
+export default corsConfig;
