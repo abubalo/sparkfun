88fceea4e2cdb5cbfc377b1576a81d8c779ce972
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-24 18:10:28 +0200

    refactor: Add return type to all user request functions

--- client/utils/queries/userQueries.ts
+++ client/utils/queries/userQueries.ts
@@ -3,25 +3,33 @@ import { UserDocument } from "../../../types";
 import { makeApiRequest } from "../helpers/makeRequest";
 
 const userAPI = axios.create({
-  baseURL: "http:localhost:3300/user",
+  baseURL: "http:localhost:3300/user/",
   withCredentials: true,
 });
 
-export const createUser = async (data: UserDocument) =>
-  makeApiRequest(() => userAPI.post("/create", data));
+export type CreateUserData = Omit<UserDocument, "role">
+export const createUser = async (
+  data: CreateUserData
+): Promise<UserDocument | null> =>
+  makeApiRequest(() => userAPI.post("create", data));
 
-type LoginData = Pick<UserDocument, "email" | "password">;
+export type LoginData = Pick<UserDocument, "email" | "password">;
 
-export const login = async (data: LoginData) =>
-  makeApiRequest(() => userAPI.post("/login", data));
+export const loginUser = async (data: LoginData): Promise<UserDocument | null> =>
+  makeApiRequest(() => userAPI.post("login", data));
 
-export const getUser = async () => makeApiRequest(() => userAPI.get(""));
+export const getUser = async (): Promise<UserDocument | null> =>
+  makeApiRequest(() => userAPI.get(""));
 
-export const getUserById = async (userId: string) =>
-  makeApiRequest(() => userAPI.get(userId));
+export const getUserById = async (
+  userId: string
+): Promise<UserDocument | null> => makeApiRequest(() => userAPI.get(userId));
 
-export const updateUser = async (userId: string, data: Partial<UserDocument>) =>
-  makeApiRequest(() => userAPI.put(`${userId}`, data));
+export const updateUser = async (
+  userId: string,
+  data: Partial<UserDocument>
+): Promise<UserDocument | null> =>
+  makeApiRequest(() => userAPI.put(userId, data));
 
 export const deleteUser = async (userId: string) =>
-  makeApiRequest(() => userAPI.delete(`${userId}`));
+  makeApiRequest(() => userAPI.delete(userId));
