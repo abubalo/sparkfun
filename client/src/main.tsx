14e750217615aa26cfb0a8a5a5753c5d9a82c3a9
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-27 12:41:51 +0200

    Set prop initial to false AnimatePresence Container in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AnimatePresence>
+        <AnimatePresence initial={false}>
           <App />
         </AnimatePresence>
       </QueryClientProvider>
