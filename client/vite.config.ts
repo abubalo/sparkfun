44fc3a05d9f460d27f3aa4d80fab6f5fb37181a4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-27 13:08:59 +0200

    chore: Configure  path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, "src"),
-      components: `${path.resolve(__dirname, "src/components")}`,
-      pages: `${path.resolve(__dirname, "src/pages")}`,
-      types: `${path.resolve(__dirname, "src/@types")}`
+      "@": path.resolve(__dirname, "./src/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@utils": path.resolve(__dirname, "./src/utils/"),
+      "@shared": path.resolve(__dirname, "./src/components/shared/"),
+      "@icons": path.resolve(__dirname, "./src/components/shared/icons/"),
+      "@queries": path.resolve(__dirname, "./src/utils/queries/"),
+      "@pages": path.resolve(__dirname, "./src/pages/"),
+      "@types": path.resolve(__dirname, "types.d.ts"),
     },
   },
-})
+});
