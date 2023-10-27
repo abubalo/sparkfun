1048d9c7c9d205b8388dbf8c190e1578c3f855cb
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-27 12:56:11 +0200

    feat: Create reusable dropdown  menu component

---
+++ client/src/components/shared/ui/dropdown/Drowpdown.tsx
@@ -0,0 +1,64 @@
+import {
+  FC,
+  KeyboardEvent,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+type Props = {
+  toggle: ReactNode;
+  content: ReactNode;
+};
+
+const Dropdown: FC<Props> = ({ toggle, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const hanldeKeydown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter" || e.key === " ") {
+      setIsOpen(!isOpen);
+    } else if (e.key === "Escape" && isOpen) {
+      setIsOpen(false);
+      dropdownRef.current?.focus();
+    }
+  };
+  useEffect(() => {}, []);
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <motion.div
+      ref={dropdownRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="relative w-max"
+    >
+      <div role="button" onKeyDown={hanldeKeydown} tabIndex={0}>
+        {toggle}
+      </div>
+
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.3 }}
+          >
+            {content}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
+
+export default Dropdown;
