5d37ece781e5a40f1554c7915b7c424de686e0d0
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-27 13:02:03 +0200

    feat: Add more middlewares

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import session from "express-session";
+import csrf from "csrf";
+import 'tsconfig-paths/register'
 
 import { config } from "./config/config";
 import { Logging } from "./utils/lib/Logging";
 
-import { authLimiter, userActionRateLimiter } from "./utils/limiter/rateLimiter";
+import {
+  authLimiter,
+  userActionRateLimiter,
+} from "./utils/limiter/rateLimiter";
 
 import userRoutes from "./user/route";
 import bookingRoutes from "./booking/route";
@@ -13,7 +21,7 @@ import reviewRoutes from "./review/route";
 
 import corsMiddleware from "./middleware/corsMiddleware";
 import requestLog from "./middleware/requestLoggingMiddleware";
-import verifyTokenMiddleware from "./middleware/verifyTokenMiddleware";
+import authToken from "./middleware/authToken";
 
 const app = express();
 
@@ -22,34 +30,38 @@ mongoose
   .connect(config.mongo.url, { retryWrites: true, w: "majority" })
   .then(() => {
     Logging.info("Connected to MongoDB!");
-    startServer();
   })
   .catch((error) => {
     Logging.error("Unable to establish Database connection!");
     Logging.error(error);
   });
 
+// Middleware to parse JSON request body
+app.use(express.json());
+// Apply compression middleware
+app.use(compression());
+// Middleware to parse cookie request header
+app.use(cookieParser());
+// app.use(session({
+//   secret: "3434vdvdsde23",
+//   resave: false,
+//   saveUninitialized: true
+// }))
+// app.use(csrf())
 // Middleware for handling CORS
 app.use(corsMiddleware);
 // Middleware for handling request logs
 app.use(requestLog);
 
-// API routes
+// // API routes
 app.use("/user", authLimiter, userRoutes);
-app.use(
-  "/bookings",
-  userActionRateLimiter,
-  verifyTokenMiddleware,
-  bookingRoutes
-);
 
-app.use("/video", userActionRateLimiter, videoRoutes);
-app.use("/messages", userActionRateLimiter);
-app.use("/review", reviewRoutes);
+app.use("/booking", userActionRateLimiter, authToken, bookingRoutes);
 
-// Start the server if mongodb is connected
-const startServer = () => {
-  app.listen(config.server.port, () => {
-    Logging.info(`Server is running on http://localhost:${config.server.port}`);
-  });
-};
+app.use("/video", userActionRateLimiter, authToken, videoRoutes);
+app.use("/messages", userActionRateLimiter, authToken);
+app.use("/review", reviewRoutes, authToken);
+
+app.listen(config.server.port, () => {
+  Logging.info(`Server is running on http://localhost:${config.server.port}`);
+});
