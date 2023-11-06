2a3caef271007f39d344104f7755259b11001910
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-06 22:48:39 +0200

    feat: Modify  auth middleware  to extract jwt token from coookie headers

--- api/src/middleware/authToken.ts
+++ api/src/middleware/authToken.ts
@@ -3,24 +3,38 @@ import { Logging } from "../utils/lib/Logging";
 import verifyToken from "../utils/auth/verifyToken";
 
 const authToken = async (
-  req: Request & { userId?: string, role?: string },
+  req: Request & { userId?: string; role?: string },
   res: Response,
   next: NextFunction
 ) => {
   try {
-    // const token = req.cookies.token;
-    const token = req.header("Authorization")?.split(' ')[1];
+    const cookies = req.headers.cookie; // Get the entire cookie string
 
-    if (!token) {
+    if (!cookies) {
+      res.status(401).json({ message: "Unauthorized: Cookies are missing" });
+      return;
+    }
+
+    // Split the cookie string into individual cookies
+    const cookiePairs = cookies.split(";");
+
+    // Find the cookie that starts with "token="
+    const tokenPair = cookiePairs.find((pair) =>
+      pair.trim().startsWith("token=")
+    );
+
+    if (!tokenPair) {
       res.status(401).json({ message: "Unauthorized: Token is missing" });
       return;
     }
 
+    // Extract the token value after the "=" sign
+    const token = tokenPair.split("=")[1];
 
-    const decodedToken = await verifyToken(token);
+    const decodedToken = await verifyToken(token.trim());
 
     if (!decodedToken) {
-      res.status(401).json("Unauthorized: token is invalid");
+      res.status(401).json({ message: "Unauthorized: Token is invalid" });
       return;
     }
 
@@ -30,7 +44,7 @@ const authToken = async (
 
     next();
   } catch (error: any) {
-    Logging.error(`Error in user middleware: ", ${error.message}`);
+    Logging.error(`Error in user middleware: ${error.message}`);
     next(error);
   }
 };
