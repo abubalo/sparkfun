6ea5c5c13029129ba29b021f5e8a5b789c2a879b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-10 21:08:27 +0200

    feat: add verify field to manage user varification status

--- api/src/user/model.ts
+++ api/src/user/model.ts
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 import { UserDocument } from "../../../types";
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  role: { type: String, default: "user" },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    role: { type: String, default: "user" },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verified: { type: Boolean, default: false },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const UserModel =
   mongoose.models.User || mongoose.model<UserDocument>("User", userSchema);
