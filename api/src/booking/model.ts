400b0c90f5327240737df8f1f92d82387ec4eb43
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-10 21:09:37 +0200

    update booking field

--- api/src/booking/model.ts
+++ api/src/booking/model.ts
@@ -1,18 +1,24 @@
 import mongoose from "mongoose";
 import { BookingDocument } from "../../../types";
 
-const bookingSchema = new mongoose.Schema({
-  user: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-  talent: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-  bookingDate: { type: Date, required: true },
-  occasion: { type: String, required: true },
-  plan: { type: String},
-  message: { type: String, required: true },
-  status: { type: String, default: "pending", required: true },
-  price: { type: Number, required: true },
-},{
-  versionKey: false
-});
+const bookingSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    talent: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    celebrant: { type: String, required: true },
+    celebrant_age: { type: Number },
+    attacthment: { type: String },
+    occasion: { type: String, required: true },
+    plan: { type: String, default: "default" },
+    message: { type: String, required: true },
+    status: { type: String, default: "pending", required: true },
+    price: { type: Number, required: true },
+    booking_date: { type: Date, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const BookingModel =
   mongoose.models.Booking ??
