d46b80ef65ddbcb860fd3440ba9410f4a39766fa
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 12:07:47 +0200

    refactor: Standardize error handling and response format in UserService

--- api/src/user/service.ts
+++ api/src/user/service.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import { MongooseError } from "mongoose";
-import { UserDocument } from "../../../types";
+import { UserDocument, Response } from "../../../types";
 import logger from "../utils/lib/logger";
 import UserModel from "./model";
 import { handleServiceError } from "../utils/error/serviceError";
@@ -13,13 +12,13 @@ export default class UserService {
     email: string,
     username: string,
     password: string
-  ): Promise<UserDocument | undefined> => {
+  ): Promise<Response<UserDocument>> => {
     try {
       const saltRounds = 10;
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
 
-      const userData = await UserModel.create({
+      const userData = await UserModel.create<UserDocument>({
         firstName,
         lastName,
         role,
@@ -28,137 +27,203 @@ export default class UserService {
         password: hashedPassword,
       });
 
-      // Log a success message
       logger.info(`User added successfully: ${userData?.email}`);
 
-      return userData;
+      return { success: true, data: userData };
     } catch (error) {
-      handleServiceError(error)
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
     }
   };
 
   public static getUserById = async (
     id: string
-  ): Promise<UserDocument | null> => {
+  ): Promise<Response<UserDocument>> => {
     try {
       const userData = await UserModel.findById(id);
 
       if (!userData) {
         logger.error(`User not found with id: ${id}`);
-        return null;
+        return { success: false, error: `User not found with id: ${id}` };
       }
 
-      logger.info(`Seccussefully retrived data with id:${id}`);
-      return userData;
+      logger.info(`Successfully retrieved data with id: ${id}`);
+      return { success: true, data: userData };
     } catch (error) {
-      if (error instanceof MongooseError) {
-        logger.error(`Error fetching user by id: ${error.message}`);
-        return null;
-      }
-      // Handle unexpexted error
-      logger.error(`Error: ${error}`);
-      return null;
+      handleServiceError(error);
+      return { success: false, error: `Something went wrong` };
     }
   };
 
   public static getUserByEmail = async (
     email: string
-  ): Promise<boolean | undefined> => {
+  ): Promise<Response<UserDocument>> => {
     try {
       const userData = await UserModel.findOne({ email });
 
-      return !!userData;
+      if (!userData) {
+        logger.error(`User not found with email: ${email}`);
+        return { success: false, error: `User not found with email: ${email}` };
+      }
+
+      logger.info(`Successfully retrieved user by email: ${email}`);
+      return { success: true, data: userData };
     } catch (error) {
-      handleServiceError(error)
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
+    }
+  };
+
+  public static getUserByUsername = async (
+    username: string
+  ): Promise<Response<UserDocument>> => {
+    try {
+      const userData = await UserModel.findOne({ username });
+
+      if (!userData) {
+        logger.error(`User not found with username: ${username}`);
+        return { success: false, error: `User not found with username: ${username}` };
+      }
+
+      logger.info(`Successfully retrieved user by username: ${username}`);
+      return { success: true, data: userData };
+    } catch (error) {
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
     }
   };
 
   public static authenticateUser = async (
     email: string,
     password: string
-  ): Promise<UserDocument | null> => {
+  ): Promise<Response<UserDocument>> => {
     try {
       const userData = await UserModel.findOne({ email });
 
       if (!userData) {
-        logger.error(`User not found with email: ${email}`);
-        return null;
+        logger.error(`User with email: ${email} does not exist`);
+        return {
+          success: false,
+          error: `User with email: ${email} does not exist`,
+        };
       }
 
       const passwordMatches = bcrypt.compareSync(password, userData.password);
 
       if (!passwordMatches) {
         logger.error(`Invalid password for user with email: ${userData.email}`);
-        return null;
+        return { success: false, error: `Invalid password` };
       }
 
-      return userData;
+      logger.info(`User authenticated successfully: ${userData.email}`);
+      return {success: true, data: userData};
     } catch (error) {
-      if (error instanceof MongooseError) {
-        logger.error(`Error authenticating user: ${error.message}`);
-        return null;
-      }
-      // Handle unexpected error
-      logger.error(`Error: ${error}`);
-      return null;
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
     }
   };
 
-  public static isUserTalent = async (userId: string): Promise<boolean | undefined > =>{
-    try {
-      const user = await UserModel.findById(userId);
-
-      if(user.role !== "talent"){
-          return false
-      }
-
-      return true
-    } catch (error) {
-      handleServiceError(error)
-    }
-  }
   public static updateUser = async (
     id: string,
     data: Partial<UserDocument>
-  ): Promise<UserDocument | null> => {
+  ): Promise<Response<UserDocument>> => {
     try {
       const userData = await UserModel.findByIdAndUpdate(id, data);
 
       if (!userData) {
         logger.error(`User not found or unable to update with id: ${id}`);
-        return null;
+        return { success: false, error: `Service Error: something went wrong` };
       }
 
-      return userData;
+      logger.info(`User updated successfully: ${userData.email}`);
+      return { success: true, data: userData };
     } catch (error) {
-      if (error instanceof MongooseError) {
-        logger.error(`Error updating user: ${error.message}`);
-        return null;
-      }
-      // Handle unexpected error
-      logger.error(`Error: ${error}`);
-      return null;
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
     }
   };
 
-  public static deleteUser = async (id: string): Promise<boolean | null> => {
+  public static updateUserRole = async (
+    id: string,
+    role: string
+  ): Promise<Response<UserDocument>> => {
+    try {
+      const userData = await UserModel.findByIdAndUpdate(id, { role });
+
+      if (!userData) {
+        logger.error(`User not found or unable to update role with id: ${id}`);
+        return {
+          success: false,
+          error: `User not found or unable to update role with id: ${id}`,
+        };
+      }
+
+      logger.info(`User role updated successfully: ${userData.email}`);
+      return { success: true, data: userData };
+    } catch (error) {
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
+    }
+  };
+
+  public static updateVerificationStatus = async (email: string) => {
+    try {
+      const user = await UserModel.findOne({ email });
+
+      if (!user) {
+        logger.error(`User with email: ${email} does not exist`);
+        return { success: false, error: `User with email: ${email} does not exist` };
+      }
+
+      user.verified = true;
+      await user.save();
+      logger.info(`User with email: ${email} has been successfully verified`);
+
+      return true;
+    }catch (error) {
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
+    }
+  };
+
+  public static isVerified = async (
+    email: string
+  ): Promise<Response<UserDocument>> => {
+    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        logger.error(`User with email: ${email} does not exist`);
+        return {
+          success: false,
+          error: `User with email: ${email} does not exist`,
+        };
+      }
+
+      return { success: user.verified };
+    } catch (error) {
+      return { success: false, error: `Service Error: something went wrong` };
+    }
+  };
+
+  public static deleteUser = async (
+    id: string
+  ): Promise<Response<UserDocument>> => {
     try {
       const userData = await UserModel.findByIdAndDelete(id);
 
       if (!userData) {
         logger.error(`User not found or unable to delete with id: ${id}`);
-        return null;
+        return {
+          success: false,
+          error: `User not found or unable to delete with id: ${id}`,
+        };
       }
 
-      return true;
+      logger.info(`User deleted successfully: ${userData.email}`);
+      return { success: true };
     } catch (error) {
-      if (error instanceof MongooseError) {
-        logger.error(`Error deleting user: ${error.message}`);
-        return null;
-      }
-      // Handle unexpected error
-      logger.error(`Error: ${error}`);
-      return null;
+      handleServiceError(error);
+      return { success: false, error: `Service Error: something went wrong` };
     }
   };
 }
