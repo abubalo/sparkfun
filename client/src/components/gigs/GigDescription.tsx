80f7a5f7d1b9ded1d333cde23507e38931c56b14
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 19:52:05 +0200

    feat: Create gig description

---
+++ client/src/components/gigs/GigDescription.tsx
@@ -0,0 +1,10 @@
+const GigDescription = () => {
+  return <div className="bg-foreground/10">
+    <div className="space-y-4">
+      <h1 className="text-slate-200 text-3xl font-bold">Happy Birthday Wishes Video</h1>
+      <p className="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus amet odio laudantium fuga quo facere, modi provident quis quae? Odio quidem rerum facere beatae velit hic nihil consequatur! Reiciendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat cum sequi ullam delectus veniam. Sit obcaecati, voluptas inventore similique ea ullam ex! Quidem totam perferendis, nostrum molestiae quis inventore tenetur.</p>
+    </div>
+  </div>;
+};
+
+export default GigDescription;
