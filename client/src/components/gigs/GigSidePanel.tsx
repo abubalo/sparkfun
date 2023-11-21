06e8c250438b98b4f1b38da20f94aa31c0924d6e
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 19:53:06 +0200

    feat: Create gig side panel

---
+++ client/src/components/gigs/GigSidePanel.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import LinkButton from "../shared/ui/button/LinkButton";
+
+const GigSidePanel = () => {
+  const [price] = useState(10);
+  return (
+    <div className="hidden h-full basis-1/3 space-y-6 md:block">
+      <div className="sticky top-4 h-auto bg-foreground p-4 rounded-lg space-y-6">
+        <div className="flex gap-4 items-center p-2">
+          {/* <Currency /> */}
+          <div className="text-textColor  space-y-4">
+            <h3 className="text-xl font-bold">Money Back Guaranteed</h3>
+            <p className=" ">
+              If you pay on web by card, we reserve the amount when you place
+              your order but only charge once you have received the video.
+            </p>
+            <p>
+              If you book a video on web with another payment method, we will
+              always provide a full refund if the celebrity doesn’t respond.
+            </p>
+          </div>
+        </div>
+        <div className="rounded-lg bg-red-400">
+          <LinkButton
+            href="book"
+            className="w-full p-4 text-lg font-semibold space-x-2"
+          >
+            Order Now! <span>${price}</span>
+          </LinkButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GigSidePanel;
