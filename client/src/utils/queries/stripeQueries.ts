c62ee48ec9186c12d242f7259f93c12d12c431f0
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 13:06:14 +0200

    feat: Create stripe payment endpoint

---
+++ client/src/utils/queries/stripeQueries.ts
@@ -0,0 +1,8 @@
+import { makeApiRequest } from "@/utils/helpers/makeRequest";
+import apiClient from "@/utils/helpers/apiClient";
+
+export const getConfigKey = () =>
+  makeApiRequest(() => apiClient.get("stripe/pub-key"));
+
+export const getClientKey = () =>
+  makeApiRequest(() => apiClient.post("stripe/create-payment-intent", {}));
