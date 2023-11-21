0bd5ae644a2514744b20868dea7ffe5ef2cf3535
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 13:29:14 +0200

    feat: Implement login page

---
+++ client/src/pages/auth/login/Login.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/components/user/loginForm/LoginForm";
+import AuthRedirect from "@/utils/auth/AuthRedirect";
+
+const Login = () => {
+  return <LoginForm />;
+};
+
+export default (Login);
