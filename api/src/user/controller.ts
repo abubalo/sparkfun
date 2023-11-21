7ff9d76aa2f260f98d66646d068b573e2488df4a
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 12:00:24 +0200

    refactor: Restructure user-related endpoint handlers for improved readability and centralized response handling

--- api/src/user/controller.ts
+++ api/src/user/controller.ts
@@ -1,48 +1,66 @@
 import { Request, Response } from "express";
-import generateToken from "utils/auth/generateToken";
+import generateToken from "../utils/auth/generateToken";
 import {
   validateLoginInput,
   validateRegistrationInput,
-} from "utils/validation/userValidation";
+} from "../utils/validation/userValidation";
 import { UserDocument } from "../../../types";
 import UserService from "./service";
 
+const handleSuccess = <T>(res: Response, data: T, status = 200) => {
+  res.status(status).json(data);
+};
+const handleFailure = <T>(res: Response, message: T, status = 500) => {
+  res.status(status).json({ error: message });
+};
+
 export const addUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const userData: UserDocument = req.body;
+    const userData = req.body;
 
     const { isValid, data, errors } = validateRegistrationInput(userData);
 
     if (!isValid) {
-      res.status(422).json({ error: errors });
-      return;
+      return handleFailure(res, errors, 422);
     }
 
     if (!data) {
-      res.status(422).json({ error: "Data is missing" });
-      return;
+      return handleSuccess(res, "Data is missing", 422);
     }
 
-    const { firstName, lastName, email, password } = data;
+    const {
+      firstName,
+      lastName,
+      email,
+      role = "user",
+      username,
+      password,
+    } = data;
 
-    const isUserExist = await UserService.getUserByEmail(email);
+    const isEmailExist = await UserService.getUserByEmail(email);
+    const isUsernameExist = await UserService.getUserByUsername(username);
 
-    if (isUserExist) {
-      res.status(409).json({ error: `User with email:${email} already exist` });
+    if (isEmailExist) {
+      handleFailure(res, `Email already exist`, 409);
       return;
+    }
+    if (isUsernameExist) {
+      return handleFailure(res, "Username is available", 409);
     }
 
     const newUser = await UserService.addUser(
       firstName,
       lastName,
+      role,
       email,
+      username,
       password
     );
 
-    res.status(201).json(newUser);
+    handleSuccess(res, newUser, 201);
   } catch (error) {
     console.log("Error in addUser:", error);
-    res.status(500).json({ error: "Internal server error" });
+    handleFailure(res, "Internal server error");
   }
 };
 
@@ -62,28 +80,45 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
-    const user = await UserService.authenticateUser(data.email, data.password);
+    // const { success, error:isVerifiedError } = await UserService.isVerified(email);
 
-    if (!user) {
-      res.status(404).json("User does not exist");
-      return;
+    // if (success === false) {
+    //   handleFailure(res, isVerifiedError, 401);
+    // }
+
+    const { data: userData, error } = await UserService.authenticateUser(
+      data.email,
+      data.password
+    );
+
+    if (!userData) {
+      return handleFailure(res, error, 404);
     }
 
-    const token = await generateToken(user);
+    const {
+      data: token,
+      success,
+      error: jwtError,
+    } = await generateToken(userData);
+
+    if (jwtError) {
+      return handleFailure(res, error, 500);
+    }
 
     res
       .status(200)
       .cookie("token", token, {
-        httpOnly: true,
+        httpOnly: true, // To prevent client-side access
         sameSite: "lax",
         secure: true,
         // domain: ".sparkfun.vercel.app",
         path: "/",
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // Cookie expires in 24 hours (in milliseconds)
       })
-      .json();
+      .json(data);
   } catch (error) {
     console.log("Error in loginUser:", error);
-    res.status(500).json("Internal server error");
+    handleFailure(res, "Internal server error");
   }
 };
 
@@ -95,20 +130,19 @@ export const getUser = async (
     const userId: string | undefined = req.userId;
 
     if (!userId) {
-      res.status(401).json({ error: "Auathorized: userId is missing" });
-      return;
+      return handleFailure(res, "Auathorized: userId is missing", 401);
     }
-    const userData = await UserService.getUserById(userId);
+    const { data, error } = await UserService.getUserById(userId);
 
-    if (!userData) {
-      res.status;
-      return;
+    if (!data) {
+      return handleFailure(res, error);
     }
 
-    res.status(200).json(userData);
+    const { _id, email, firstName, lastName, username } = data;
+
+    handleSuccess(res, { id: _id, email, firstName, lastName, username });
   } catch (error) {
-    console.log("Error in getUser:", error);
-    res.status(500).json("Internal server error");
+    handleFailure(res, "Internal server error");
   }
 };
 
@@ -120,17 +154,43 @@ export const updateUser = async (
     const userId: string = req.params.id;
     const updateData: Partial<UserDocument> = req.body;
 
-    const userData = await UserService.updateUser(userId, updateData);
+    const { data } = await UserService.updateUser(userId, updateData);
 
-    if (!userData) {
-      res.status(404).json({ error: "User not found" });
-      return;
+    if (!data) {
+      return handleFailure(res, "User not found", 404);
     }
 
-    res.status(200).json(userData);
+    handleSuccess(res, data);
   } catch (error) {
-    console.log("Error in updatUser:", error);
-    res.status(500).json("Internal server error");
+    handleFailure(res, "Internal server error");
+  }
+};
+
+export const updateUserRole = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const { role } = req.body;
+
+    if (!userId) {
+      return handleFailure(res, "Invalid paramter", 422);
+    }
+
+    const { success } = await UserService.getUserById(userId);
+
+    if (success === false) {
+      return handleFailure(res, "User Does not exist", 404);
+    }
+
+    const { data, error } = await UserService.updateUserRole(userId, role);
+
+    if (!data) {
+      return handleFailure(res, error);
+    }
+
+    handleSuccess(res, "User role successfully updated", 204);
+  } catch (error) {
+    console.log("Error in updatUserRole:", error);
+    handleFailure(res, "Internal server error");
   }
 };
 
@@ -143,15 +203,15 @@ export const deleteUser = async (
     const isDelete = await UserService.deleteUser(userId);
 
     if (!isDelete) {
-      res
-        .status(500)
-        .json({ error: "Error: Unable to delete user, please try again" });
-      return;
+      return handleFailure(
+        res,
+        "Error: Unable to delete user, please try again"
+      );
     }
 
-    res.status(200).json({ error: "User successfully  deleted!" });
+    handleSuccess(res, "User successfully  deleted!", 204);
   } catch (error) {
     console.error("Error in deleteUser:", error);
-    res.status(500).json({ error: "Internal server error" });
+    handleFailure(res, "Internal server error");
   }
 };
