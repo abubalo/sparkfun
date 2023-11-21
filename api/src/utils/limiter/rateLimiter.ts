063c234fb6a4ffa2ca2c519be19fe1dd1cb18165
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 12:18:15 +0200

    Increase the rate limit to 1 hour

--- api/src/utils/limiter/rateLimiter.ts
+++ api/src/utils/limiter/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 // Create a rate limiter for authentication and registration endpoints
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 100, // 15 minutes window
+  windowMs: 60 * 60 * 100, // 1 hour window
   max: 5, // Limit each IP to 5 requests per windowMs
   message: "Too many login or registration attempts, please try again later.",
 });
@@ -17,7 +17,7 @@ export const apiRateLimiter = rateLimit({
 // Create a rate limiter for user actions (e.g., bookings, messages)
 export const userActionRateLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 10, // Number of actions allowed in the defined windowMs
+  max: 100, // Number of actions allowed in the defined windowMs
   message:
     "You've exceeded the rate limit for this action. Please try again later.",
 });
