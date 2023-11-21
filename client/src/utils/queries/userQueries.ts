c06f6188403f751174dfe8dc2b74d08387d4046d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 13:03:32 +0200

    tions into a centralized location and replaced dirrefactor: Consolidate API endpoints and utilize centralized API client
    Merged user-related API funcect axios calls with a single API client. This refactor aims to improve maintainability and consistency in making API requests across the application.

--- client/src/utils/queries/userQueries.ts
+++ client/src/utils/queries/userQueries.ts
@@ -1,48 +1,46 @@
-import axios from "axios";
 import { UserDocument } from "../../../../types";
+import apiClient from "../helpers/apiClient";
 import { makeApiRequest } from "../helpers/makeRequest";
 
-const userAPI = axios.create({
-  baseURL: "http://localhost:5000/user/",
-  withCredentials: true,
-});
+
 
 export type CreateUserData = Omit<UserDocument, "role">;
+
 export const createUser = async (
   data: CreateUserData
 ): Promise<UserDocument | null> =>
-  makeApiRequest(() => userAPI.post("signup", data));
+  makeApiRequest(() => apiClient.post("/user/signup", data));
 
 export type LoginData = Pick<UserDocument, "email" | "password">;
 
 export const verifyAccount = async (
   token: string
 ): Promise<UserDocument | null> =>
-  makeApiRequest(() => userAPI.post(`verify?token=${token}`));
+  makeApiRequest(() => apiClient.post(`/user/verify?token=${token}`));
 
 export const loginUser = async (
   data: LoginData
 ): Promise<UserDocument | null> =>
-  makeApiRequest(() => userAPI.post("login", data));
+  makeApiRequest(() => apiClient.post("/user/login", data));
 
 export const getUser = async (): Promise<UserDocument | null> =>
-  makeApiRequest(() => userAPI.get(""));
+  makeApiRequest(() => apiClient.get("/user/"));
 
 export const getUserById = async (
   userId: string
-): Promise<UserDocument | null> => makeApiRequest(() => userAPI.get(userId));
+): Promise<UserDocument | null> => makeApiRequest(() => apiClient.get(`/${userId}`));
 
 export const updateUser = async (
   userId: string,
   data: Partial<UserDocument>
 ): Promise<UserDocument | null> =>
-  makeApiRequest(() => userAPI.put(userId, data));
+  makeApiRequest(() => apiClient.put(`/user/${userId}`, data));
 
 export const deleteUser = async (userId: string) =>
-  makeApiRequest(() => userAPI.delete(userId));
+  makeApiRequest(() => apiClient.delete(`/user/${userId}`));
 
 export const forgotPassword = async (val: { email: string }) =>
-  makeApiRequest(() => userAPI.post("forgot-password", { val }));
+  makeApiRequest(() => apiClient.post("/user/forgot-password", { val }));
 
 export const updateUserRole = async (data: { userId: string; role: string }) =>
-  makeApiRequest(() => userAPI.post(`update-role/${data.userId}`, { data }));
+  makeApiRequest(() => apiClient.post(`/user/update-role/${data.userId}`, { data }));
