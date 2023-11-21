a04c2216c734fb318fd0024b0c9a70d03aeb01bb
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 12:40:00 +0200

    feat: Refactor Button component with basic Tailwind styling and hover effect

--- client/src/components/shared/ui/button/Button.tsx
+++ client/src/components/shared/ui/button/Button.tsx
@@ -4,24 +4,30 @@ import { FC, ReactNode } from "react";
 type Props<T = unknown> = {
   type: "submit" | "reset" | "button";
   children: ReactNode;
+  id?: string;
   className?: string;
+  disabled?: boolean;
   onClick?: (val: T) => void;
   onHover?: (val: T) => void;
 };
 const Button: FC<Props> = ({
   type = "submit",
   children,
+  id,
+  disabled = false,
   className,
   onClick,
   onHover,
 }) => {
   className = twMerge(
-    ` "w-full font-bold leading-6 text-white font-white semibold px-4 py-2 bg-primary rounded-md hover:bg-purple-800 transition-all ease-in-out" ${
+    ` "w-full font-bold leading-6 text-white font-white semibold px-4 py-2 bg-primary rounded-md hover:bg-purple-700 active:scale-[1.03] transition-all ease-in-out duration-75" ${
       className ?? ""
     }`
   );
   return (
     <button
+      id={id}
+      disabled={disabled}
       type={type}
       onClick={onClick}
       className={className}
