454408577bbc5d0e5a746e7c1d07574b9bed9f0e
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-21 12:30:25 +0200

    feat: Restructure React Router setup and paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,41 @@
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
-import HomePage from "./pages/home/Homepage";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import Dashboard from "./pages/dashboard/Dashboard";
-import TalentProfiles from "./pages/talent/TalentProfiles";
-import Talent from "./pages/talent/Talent";
+import HomePage from "@/pages/home/Homepage";
+import Login from "@/pages/auth/login/Login";
+import Signup from "@/pages/auth/signup/Signup";
+import Dashboard from "@/pages/dashboard/Dashboard";
+import TalentProfile from "@/pages/talent/TalentProfile";
+import BrowseTalents from "@/pages/talent/BrowseTalents";
+import Account from "@/pages/account/Account";
+import ResetPassword from "@/pages/auth/resetPassword/ResetPassword";
+import Settings from "@/pages/settings/Settings";
+import Layout from "./Layout";
+import NotFoundPage from "@/pages/notFoundPage/NotFoundPage";
+import VerifyAcccount from "./pages/auth/accountVerification/VerifyAcccount";
+import Booking from "@/pages/booking/Booking";
+import Checkout from "@/pages/checkout/Checkout";
+import GapPage from "@/pages/gig/GigPage";
+import Payment from "./components/payment/Payment";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:3300";
-  axios.defaults.withCredentials = true;
-
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/talents" element={<TalentProfiles />} />
-      <Route path="/talent/:id" element={<Talent />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/reset" element={<ResetPassword />} />
+        <Route path="/profile" element={<Account />} />
+        <Route path="/profile/settings" element={<Settings />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/browse" element={<BrowseTalents />} />
+        <Route path="/:profile" element={<TalentProfile />} />
+        <Route path="/browse/:gigId" element={<GapPage />} />
+        <Route path="/browse/:id/book" element={<Booking />} />
+        <Route path="/browse/:id/book/checkout" element={<Checkout />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/profile/earnings" element={<VerifyAcccount />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 }
