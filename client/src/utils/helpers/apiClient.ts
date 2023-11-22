69f45e694a67d135812bf33f57937d63bb2d231c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-22 22:45:55 +0200

    refactor: Remove interceptor from apiClient file

--- client/src/utils/helpers/apiClient.ts
+++ client/src/utils/helpers/apiClient.ts
@@ -8,21 +8,5 @@ const apiClient = axios.create({
   },
 });
 
-// Interceptor to include the JWT token in the headers
-apiClient.interceptors.request.use((config) => {
-  const token = getCookie("token")/* Get the token from where it's stored (e.g., local storage) */;
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return config;
-});
-
-// Function to read a cookie by name
-function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift();
-  // Use optional chaining to handle null
-}
 
 export default apiClient;
