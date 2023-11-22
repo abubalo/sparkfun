6635a571df1eb3be2c081a5b21739342d1959f03
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-22 22:32:42 +0200

    feat: Define action types of all states

---
+++ client/src/utils/redux/actions/actionTypes.ts
@@ -0,0 +1,5 @@
+export const ADD_USER = 'ADD_USER';
+export const GET_USER = 'GET_USER';
+export const ADD_BOOKING = 'ADD_BOOKING';
+export const GET_BOOKING = 'GET_BOOKING';
+export const UPDATE_BOOKING_STATUS = 'UPDATE_BOOKING_STATUS';
