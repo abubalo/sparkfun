53b44aa35bab4c8ad8b8e676c434d191eb1dc980
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-01 23:05:39 +0200

    refactor: Rearange routes for better exprerience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "@/pages/home/Homepage";
-import Login from "@/pages/auth/login/Login";
-import Signup from "@/pages/auth/signup/Signup";
-import Dashboard from "@/pages/dashboard/Dashboard";
-import TalentProfile from "@/pages/marketplace/TalentProfile";
-import BrowseTalents from "@/pages/marketplace/BrowseTalents";
-import Account from "@/pages/account/Account";
-import ResetPassword from "@/pages/auth/resetPassword/ResetPassword";
-import Settings from "@/pages/settings/Settings";
+import HomePage from "@pages/home/Homepage";
+import Login from "@pages/auth/login/Login";
+import Signup from "@pages/auth/signup/Signup";
+import Dashboard from "@pages/dashboard/Dashboard";
+import TalentProfile from "@pages/marketplace/TalentProfile";
+import BrowseTalents from "@pages/marketplace/BrowseTalents";
+import Account from "@pages/account/Account";
+import ResetPassword from "@pages/auth/resetPassword/ResetPassword";
+import Settings from "@pages/settings/Settings";
 import Layout from "./Layout";
-import NotFoundPage from "@/pages/notFoundPage/NotFoundPage";
+import NotFoundPage from "@pages/notFoundPage/NotFoundPage";
 import VerifyAcccount from "./pages/auth/accountVerification/VerifyAcccount";
-import Booking from "@/pages/booking/Booking";
-import Checkout from "@/pages/checkout/Checkout";
-import GapPage from "@/pages/gig/GigPage";
+import Booking from "@pages/booking/Booking";
+import Checkout from "@pages/checkout/Checkout";
+import GapPage from "@pages/gig/GigPage";
 import Payment from "./components/payment/Payment";
 
 function App() {
@@ -30,8 +30,8 @@ function App() {
         <Route path="/browse" element={<BrowseTalents />} />
         <Route path="/:profile" element={<TalentProfile />} />
         <Route path="/browse/:gigId" element={<GapPage />} />
-        <Route path="/browse/:id/book" element={<Booking />} />
-        <Route path="/browse/:id/book/checkout" element={<Checkout />} />
+        <Route path="/book" element={<Booking />} />
+        <Route path="book/checkout" element={<Checkout />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/profile/earnings" element={<VerifyAcccount />} />
         <Route path="*" element={<NotFoundPage />} />
