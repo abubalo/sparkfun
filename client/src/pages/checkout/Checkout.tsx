668b21e4548282f54b63b5dc8b571ea51004984a
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-02 10:31:17 +0200

    refactor: Update import path to use abosulte alias

--- client/src/pages/checkout/Checkout.tsx
+++ client/src/pages/checkout/Checkout.tsx
@@ -1,9 +1,9 @@
-import BookingPanel from "@/components/booking/bookingForms/BookingPanel";
-import CheckOutPage from "@/components/checkout/CheckOutPage";
+import BookingPanel from "@components/booking/bookingForms/BookingPanel";
+import CheckOutPage from "@components/checkout/CheckOutPage";
 
 const Checkout = () => {
   return (
-    <main className="container mx-auto relative h-auto flex flex-col-reverse gap-6 p-8 md:flex-row md:p-0">
+    <main className="container relative flex flex-col-reverse h-auto gap-6 p-8 mx-auto md:flex-row md:p-0">
       <CheckOutPage />
       <BookingPanel />
     </main>
