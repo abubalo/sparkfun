67f7bb377d21475cf28aed6ec4cb1c2b5865d262
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-02 10:29:21 +0200

    Refactor: Updated import paths to use absolute aliases for better readability and maintainability

--- client/src/pages/booking/Booking.tsx
+++ client/src/pages/booking/Booking.tsx
@@ -1,12 +1,12 @@
-import BookingPanel from "@/components/booking/bookingForms/BookingPanel";
-import RenderBookingForm from "@/components/booking/bookingForms/RenderBookingForm";
+import BookingPanel from "@components/booking/bookingForms/BookingPanel";
+import RenderBookingForm from "@components/booking/bookingForms/RenderBookingForm";
 
 const Booking = () => {
   return (
-    <main className="container mx-auto flex flex-col h-auto p-4   md:flex-row-reverse md:-p-0  md:gap-8">
+    <main className="container flex flex-col h-auto p-4 mx-auto md:flex-row-reverse md:-p-0 md:gap-8">
       <BookingPanel />
       <RenderBookingForm />
-    </main>
+  </main>
   );
 };
 
