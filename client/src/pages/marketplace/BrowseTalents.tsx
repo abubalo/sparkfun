f2a8e536546b7ac855806eff7d862d4820ca2ea9
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-02 11:10:13 +0200

    refactor: Add keys to components

--- client/src/pages/marketplace/BrowseTalents.tsx
+++ client/src/pages/marketplace/BrowseTalents.tsx
@@ -1,6 +1,6 @@
-// import SearchFilter from "@/components/shared/ui/search/SearchFilter";
-import { img1, img2, img3, img4, img5 } from "@/components/Temp";
-import ProfilePicture from "@/components/user/profilePicture/ProfilePicture";
+// import SearchFilter from "@components/shared/ui/search/SearchFilter";
+import { img1, img2, img3, img4, img5 } from "@components/Temp";
+import ProfilePicture from "@components/user/profilePicture/ProfilePicture";
 import BrowseTalentCard from "./BrowseTalentCard";
 
 const BrowseTalents = () => {
@@ -12,9 +12,9 @@ const BrowseTalents = () => {
     <main className="container mx-auto md:space-y-16">
       <div>
         <h2 className="text-2xl font-bold">Explore</h2>
-        <div className="flex gap-10 justify-center ">
-          {categories.map((category) => (
-            <div className="flex flex-col items-center text-center gap-3">
+        <div className="flex justify-center gap-10 ">
+          {categories.map((category, i) => (
+            <div key={i} className="flex flex-col items-center gap-3 text-center">
               <ProfilePicture />
               <div>
                 <p className="font-medium">{category}</p>
@@ -24,7 +24,7 @@ const BrowseTalents = () => {
         </div>
         {/* <SearchFilter /> */}
       </div>
-      <div className="grid  grid-cols-2 mt-6 h-full gap-6 md:grid-cols-3">
+      <div className="grid h-full grid-cols-2 gap-6 mt-6 md:grid-cols-3">
         <BrowseTalentCard
           name="John Smith"
           title="Comedian"
@@ -32,6 +32,7 @@ const BrowseTalents = () => {
           userUrl={"johnsmith"}
           url="adjjas"
           stars={5}
+          key={1}
         />
         <BrowseTalentCard
           name="Ruben Gasslow"
@@ -40,6 +41,7 @@ const BrowseTalents = () => {
           userUrl={"rubengasslow"}
           url="dffs"
           stars={5}
+          key={2}
         />
         <BrowseTalentCard
           name="Jora Allan"
@@ -48,6 +50,7 @@ const BrowseTalents = () => {
           userUrl={"joraallan"}
           url="sdfsd"
           stars={5}
+          key={3}
         />
         <BrowseTalentCard
           name="Tope Bose"
@@ -56,6 +59,7 @@ const BrowseTalents = () => {
           userUrl={"topebose"}
           url="bbdsfvsge"
           stars={5}
+          key={4}
         />
         <BrowseTalentCard
           name="Lilla Greenwood"
@@ -64,6 +68,7 @@ const BrowseTalents = () => {
           userUrl={"johnsmith"}
           url="gbrfdfgf"
           stars={5}
+          key={5}
         />
       </div>
     </main>
