ae3a5fe13af42f63794d01d95b69fe24cd56e09f
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-02 09:45:24 +0200

    Refactor: Updated import paths to use absolute aliases for better readability and maintainability

--- client/src/pages/account/Account.tsx
+++ client/src/pages/account/Account.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import RequiredAuth from "@/utils/auth/RequiredAuth";
-import TalentAccount from "@/components/talent/TalentAccount";
-import UserAccount from "@/components/user/userAccount/UserAccount";
+import RequiredAuth from "@utils/auth/RequiredAuth";
+import TalentAccount from "@components/talent/TalentAccount";
+import UserAccount from "@components/user/userAccount/UserAccount";
 
 const Account = () => {
   const [role] = useState("talent");
