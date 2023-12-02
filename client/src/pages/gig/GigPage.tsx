34bdecd93745ff9ace71fff7a753a05e971e04f6
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-02 10:52:19 +0200

    refactor: Update import path to use abosulte aliases

--- client/src/pages/gig/GigPage.tsx
+++ client/src/pages/gig/GigPage.tsx
@@ -1,13 +1,21 @@
-import GigSidePanel from "@/components/gigs/GigSidePanel";
-import GigDescription from "@/components/gigs/GigDescription";
-import VideoPlayer from "@/components/video/VideoPlayer";
-import RecentlyVisited from "@/components/gigs/RecentlyVisited";
-import GigCard from "@/components/gigs/GigCard";
+import GigSidePanel from "@components/gigs/GigSidePanel";
+import GigDescription from "@components/gigs/GigDescription";
+import VideoPlayer from "@components/video/VideoPlayer";
+import RecentlyVisited from "@components/gigs/RecentlyVisited";
+import GigCard from "@components/gigs/GigCard";
+import { setGigId } from "@utils/redux/reducers/gigIdSlice";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const GigPage = () => {
+  const dispatch = useDispatch();
+  const {gigId} = useParams();
+  dispatch(setGigId(gigId));
+
+
   return (
     <div className="w-full p-4 md:flex md:w-4/5 md:gap-10 md:mx-auto">
-      <div className="md:basis-4/5 md:w-max space-y-8">
+      <div className="space-y-8 md:basis-4/5 md:w-max">
         <div className="w-full">
           <VideoPlayer
             src={
