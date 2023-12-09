3d93ed4f13f6ac9d4b55f62de478a29dfc6f1541
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-09 19:09:17 +0200

    update zod schema to reflect on chnaged data

--- api/src/utils/validation/bookingValidation.ts
+++ api/src/utils/validation/bookingValidation.ts
@@ -1,15 +1,18 @@
+import mongoose from "mongoose";
 import { z, ZodError, TypeOf } from "zod";
 import { BookingDocument } from "../../../../types";
 
 const createBookingSchema = z.object({
-  user: z.string(),
-  talent: z.string(),
-  bookingDate: z.date(),
-  occasion: z.string(),
-  package: z.string(),
+  user: z.custom<mongoose.Types.ObjectId>(),
+  gigId: z.custom<mongoose.Types.ObjectId>(),
+  celebrant: z.string(),
+  celebrantAge: z.string(),
   message: z.string(),
-  status: z.enum(["pending", "completed", "modification", "cancel"]),
+  bookingDate: z.union([z.date(), z.string()]),
+  occasion: z.string(),
+  plan: z.string().optional(),
   price: z.number(),
+  status: z.string().default('pending'),
 });
 
 
