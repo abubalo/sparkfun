74e369754d2b87b9825f27f71662061a57bae90c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-09 18:55:58 +0200

    refactor: Remove commented codes

--- api/src/payment/payment.ts
+++ api/src/payment/payment.ts
@@ -14,19 +14,8 @@ export const proccessPayment = async (req: Request, res: Response) => {
       },
     });
 
-    // await stripe.paymentIntents.capture(paymentIntents.id);
-
     res.status(200).send({client_secret: paymentIntents.client_secret});
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 };
-
-// const { amount, token, currency, paymentMethod } = req.body;
-
-// const paymentIntents = await stripe.paymentIntents.create({
-//   amount,
-//   currency,
-//   payment_method_types: paymentMethod,
-//   payment_method_data: token,
-// });
