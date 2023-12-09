d814e8fd2165230b3ae0f060072a9f2f8892b1a2
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-09 20:09:38 +0200

    feat: Add redux and hooks path aliases in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,14 +7,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@types": path.resolve(__dirname, "../types/"),
       "@": path.resolve(__dirname, "./src/"),
       "@components": path.resolve(__dirname, "./src/components/"),
       "@utils": path.resolve(__dirname, "./src/utils/"),
       "@shared": path.resolve(__dirname, "./src/components/shared/"),
       "@icons": path.resolve(__dirname, "./src/components/shared/icons/"),
       "@queries": path.resolve(__dirname, "./src/utils/queries/"),
+      "@redux": path.resolve(__dirname, "./src/utils/redux/"),
+      "@hooks": path.resolve(__dirname, "./src/utils/hooks/"),
       "@pages": path.resolve(__dirname, "./src/pages/"),
-      "@types": path.resolve(__dirname, "types.d.ts"),
     },
   },
 });
